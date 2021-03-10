9e3d6ffdfffb6836ea9c58043fb1b278a684076c
DrNiels <nt@symcon.de>
2021-03-10 09:43:43 +0100

    Test Devices for Configurator Webinar

---
+++ TestDevice1/module.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+    class TestDevice1 extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyInteger("DeviceID", 0);
+            $this->RegisterDeviceType("SecondaryID", 0);
+        }
+
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
+
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+        }
+    }
---
+++ TestDevice2/module.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+    class TestDevice2 extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RequireParent('{63D1D28F-6901-8BB7-8C8E-1046022EFEB4}');
+            
+            $this->RegisterPropertyInteger("DeviceID", 0);
+            $this->RegisterDeviceType("SecondaryID", 0);
+        }
+
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
+
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+        }
+    }
